2a5a6bc163b218de8154c348cd8e2d7ddd8a5e74
Brian M. Viana <brianmviana@gmail.com>
2018-12-01 02:30:12 -0300

    configurando broker

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const serverMQTT = require('./src/mqtt/broker');
 
 //Iniciando APP
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use(serverMQTT);
 
 //Iniciando o DB
 mongoose.connect('mongodb://localhost/nokixstl', { useNewUrlParser: true, useCreateIndex: true });
---
+++ src/mqtt/broker.js
@@ -0,0 +1,24 @@
+var mosca = require('mosca');
+const express = require('express');
+
+const serverMQTT = () =>{
+  var pubsubsettings = {
+    type: 'mqtt',
+    json: true,
+    mqtt: require('mqtt'),
+    host: '192.168.0.166',
+    port: 1883
+  };
+
+  var settings = {
+    port:1883,
+    host:'192.168.0.166'//IP DO SERVIDOR
+  }
+
+  var server = new mosca.Server(settings);
+  server.on('ready', function(){
+    console.log("ready");
+  });
+
+}
+module.exports = serverMQTT;
